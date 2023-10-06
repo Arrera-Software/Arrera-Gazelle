c4d670e26156bafcf670fe7b4dd3ce4052fae4f5
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-10-06 15:29:47 +0200

    objet createfile

---
+++ createFile.py
@@ -0,0 +1,21 @@
+
+class createFileJSON :
+    def __init__(self) :
+        self.configFileNetwork = {"name" : "",
+                                    "lang" :"",
+                                    "nombreVilleMeteo":"",
+                                    "lieuDomicile":"",
+                                    "lieuTravail":"",
+                                    "iconAssistant":"",
+                                    "emplacementAssetHorloge":"",
+                                    "interfaceColor" : "",
+                                    "interfaceTextColor":"",
+                                    "utilisationVous":"",
+                                    "bute":"",
+                                    "createur":"",
+                                    "listFonction":[],
+                                    "toucheCalculatrice":"",
+                                    "moteurRechercheDefault":""
+                                    }
+    
+    
