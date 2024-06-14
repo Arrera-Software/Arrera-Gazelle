1a28497ce3d898b3abc068720cbcb60d0f42e562
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-06-14 23:05:22 +0200

    Peit oublie

--- objet/arreraGazelle.py
+++ objet/arreraGazelle.py
@@ -3,7 +3,8 @@ from librairy.dectectionOS import*
 from librairy.gestionSoftWindows import*
 
 class CArreraGazelle :
-    def __init__(self,emplacementJsonUser:str,emplacementJsonNeuronNetwork:str,emplacementJsonAssistant:str):
+    def __init__(self,emplacementJsonUser:str,emplacementJsonNeuronNetwork:str,emplacementJsonAssistant:str,emplacementFileSetting:str):
         self.__fileJsonUser = jsonWork(emplacementJsonUser)
         self.__fileJsonNeuronNetwork = jsonWork(emplacementJsonNeuronNetwork)
         self.__fileJsonAssistant = jsonWork(emplacementJsonAssistant)
+        self.__fileJsonSetting = jsonWork(emplacementFileSetting)
