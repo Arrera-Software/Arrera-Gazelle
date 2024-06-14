cfef1fc6febcb7386f440b2865999c46751c9396
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-06-14 23:02:53 +0200

    rerpise du projet

---
+++ objet/arreraGazelle.py
@@ -0,0 +1,9 @@
+from librairy.travailJSON import*
+from librairy.dectectionOS import*
+from librairy.gestionSoftWindows import*
+
+class CArreraGazelle :
+    def __init__(self,emplacementJsonUser:str,emplacementJsonNeuronNetwork:str,emplacementJsonAssistant:str):
+        self.__fileJsonUser = jsonWork(emplacementJsonUser)
+        self.__fileJsonNeuronNetwork = jsonWork(emplacementJsonNeuronNetwork)
+        self.__fileJsonAssistant = jsonWork(emplacementJsonAssistant)
