f2b6c6a1f7a0e050644f3308acd421b34dfd08bb
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-06-15 10:05:20 +0200

    Ajout changement name et genre

---
+++ main.py
@@ -0,0 +1,3 @@
+from objet.arreraGazelle import*
+
+gazelle = CArreraGazelle("FileJSON/configUser.json","FileJSON/configNeuron.json","FileJSON/sixConfif.json")
--- objet/arreraGazelle.py
+++ objet/arreraGazelle.py
@@ -3,8 +3,13 @@ from librairy.dectectionOS import*
 from librairy.gestionSoftWindows import*
 
 class CArreraGazelle :
-    def __init__(self,emplacementJsonUser:str,emplacementJsonNeuronNetwork:str,emplacementJsonAssistant:str,emplacementFileSetting:str):
+    def __init__(self,emplacementJsonUser:str,emplacementJsonNeuronNetwork:str,emplacementJsonAssistant:str):
         self.__fileJsonUser = jsonWork(emplacementJsonUser)
         self.__fileJsonNeuronNetwork = jsonWork(emplacementJsonNeuronNetwork)
         self.__fileJsonAssistant = jsonWork(emplacementJsonAssistant)
-        self.__fileJsonSetting = jsonWork(emplacementFileSetting)
+    
+    def changeUserName(self,newName:str):
+        self.__fileJsonUser.EcritureJSON("user",newName)
+    
+    def changeUserGenre(self,newGenre:str):
+        self.__fileJsonUser.EcritureJSON("genre",newGenre)
