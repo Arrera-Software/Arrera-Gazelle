fd5a940a081c87fddbeda2d7f40792c7f0efc400
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-06-16 10:42:48 +0200

    Ajout swicht pour lancer sois en dark ou light

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ oldGui = CArreraGazelleUI(windows,"FileJSON/configUser.json",
                           "FileJSON/configNeuron.json",
                           "FileJSON/sixConfif.json",
                           "FileJSON/configSetting.json")
-oldGui.active(True)
+mode = int
+
+mode = int(input("1 : Dark \n2 : Light \n#"))
+
+match mode :
+    case 1 : 
+        oldGui.active(True)
+    case 2 :
+        oldGui.active(False)
+
 windows.mainloop()
 
 #"FileJSON/configUser.json","FileJSON/configNeuron.json","FileJSON/sixConfif.json"
