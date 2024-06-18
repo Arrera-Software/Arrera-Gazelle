5aab990200550d2734891bb3c1538b8bf4eebc53
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-06-18 10:58:42 +0200

    Commencement

--- gui/ArreraGazelleUI.py
+++ gui/ArreraGazelleUI.py
@@ -11,9 +11,21 @@ class CArreraGazelleUI :
         # recuperation color
         self.__listMainColor = [jsonSetting.lectureJSON("lightColor"),jsonSetting.lectureJSON("darkColor")]
         self.__listMainTextColor = [jsonSetting.lectureJSON("lightTextColor"),jsonSetting.lectureJSON("darkTextColor")]
+        # Mise de la fenetre dans un atribut
+        self.__windows = windows
+        # Declaration des cardre
+        self.__mainCadre = Frame(self.__windows,width=500,height=400)
+
+        # Widget 
+        # Main frame 
+        btnAcceuilUser = Button(self.__mainCadre,width=8,height=4,text="Utilisateur",font=("arial","15"))
+
+        # Affichage 
+        btnAcceuilUser.place(x=20,y=20)
     
     def active(self,darkMode:bool):
         if (darkMode == True) :
             nb = 1
         else : 
             nb = 0 
+        self.__mainCadre.pack()
