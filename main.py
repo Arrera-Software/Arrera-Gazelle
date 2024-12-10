72ebd6e9a82e6fb5dbbf18b6d8aa738867694fb8
Baptiste P <baptiste.pauchet13@gmail.com>
2024-12-10 09:59:21 +0100

    Réecriture du main

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 from gui.ArreraGazelleUIOld import*
 from gui.ArreraGazelleUI import*
 
-windows = Tk()
 
-def quit():
+
+def quit(windows):
     windows.destroy()
 
-mode = int 
+def main():
+    windows = Tk()
+    mode = int(input("1. Old\n2.New\n# "))
+    match mode :
+        case 1 :
+            gui = CArreraGazelleUIOld(windows,"FileJSON/configUser.json",
+                                    "FileJSON/configNeuron.json",
+                                    "FileJSON/sixConfig.json",
+                                    "FileJSON/configOldSetting.json")
+            gui.passQuitFnc(quit)
+        case 2 :
+            gui = CArreraGazelleUI(windows,"FileJSON/configUser.json",
+                                    "FileJSON/configNeuron.json",
+                                    "FileJSON/sixConfig.json",
+                                    "FileJSON/configNewSetting.json")
+            windows.maxsize(500,400)
+            windows.minsize(500,400)
+        case other :
+            print("Invalid input")
+            return
 
-mode = int(input("1. Old\n2.New\n# "))
+    mode = int(input("1 : Dark \n2 : Light \n# "))
 
-match mode :
-    case 1 :
-        gui = CArreraGazelleUIOld(windows,"FileJSON/configUser.json",
-                                "FileJSON/configNeuron.json",
-                                "FileJSON/sixConfig.json",
-                                "FileJSON/configOldSetting.json")
-        gui.passQuitFnc(quit)
-    case 2 : 
-        gui = CArreraGazelleUI(windows,"FileJSON/configUser.json",
-                                "FileJSON/configNeuron.json",
-                                "FileJSON/sixConfig.json",
-                                "FileJSON/configNewSetting.json")
-        windows.maxsize(500,400)
-        windows.minsize(500,400)
+    match mode :
+        case 1 :
+            gui.active(True)
+        case 2 :
+            gui.active(False)
 
-mode = int(input("1 : Dark \n2 : Light \n# "))
+    windows.mainloop()
 
-match mode :
-    case 1 : 
-        gui.active(True)
-    case 2 :
-        gui.active(False)
-
-windows.mainloop()
-
-#"FileJSON/configUser.json","FileJSON/configNeuron.json","FileJSON/sixConfif.json"
+if __name__ == "__main__":
+    main()
