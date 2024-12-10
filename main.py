78284d419d638efc0baf1e3c1fd179af511c9ec4
Baptiste P <baptiste.pauchet13@gmail.com>
2024-12-10 10:03:12 +0100

    Renommange des classe

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from gui.ArreraGazelleUIOld import*
-from gui.ArreraGazelleUI import*
-
-
+from gui.CArreraGazelleUIRyleyCopilote import*
+from gui.ArreraGazelleUISix import*
 
 def quit(windows):
     windows.destroy()
 
 def main():
     windows = Tk()
-    mode = int(input("1. Old\n2.New\n# "))
+    mode = int(input("1. Ryley/Copilote \n2. Six \n# "))
     match mode :
         case 1 :
-            gui = CArreraGazelleUIOld(windows,"FileJSON/configUser.json",
+            gui = CArreraGazelleUIRyleyCopilote(windows, "FileJSON/configUser.json",
                                     "FileJSON/configNeuron.json",
                                     "FileJSON/sixConfig.json",
                                     "FileJSON/configOldSetting.json")
             gui.passQuitFnc(quit)
         case 2 :
-            gui = CArreraGazelleUI(windows,"FileJSON/configUser.json",
+            gui = CArreraGazelleUISix(windows, "FileJSON/configUser.json",
                                     "FileJSON/configNeuron.json",
                                     "FileJSON/sixConfig.json",
                                     "FileJSON/configNewSetting.json")
