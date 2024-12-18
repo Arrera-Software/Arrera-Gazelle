ce9b8422b9f6775dda61b9fa925d2e6e64088249
Baptiste P <baptiste.pauchet13@gmail.com>
2024-12-18 18:45:53 +0100

    Fin de la partie utilisateur

--- gui/ArreraGazelleUISix.py
+++ gui/ArreraGazelleUISix.py
@@ -15,20 +15,27 @@ class CArreraGazelleUISix :
         self.__mainCadre = self.__arrtk.createFrame(self.__windows,width=500,height=400)
 
         self.__userFrame = self.__arrtk.createFrame(self.__windows,width=500,height=330)
+        self.__userAcceuil = self.__arrtk.createFrame(self.__userFrame,width=500,height=330)
+        self.__userName = self.__arrtk.createFrame(self.__userFrame,width=500,height=330)
+        self.__userGenre = self.__arrtk.createFrame(self.__userFrame,width=500,height=330)
 
         self.__backFrame = self.__arrtk.createFrame(self.__windows,width=500,height=70)
 
         # Variable
         # Taille Police
         taillePolice = 20
+        # Liste
+        listGenre = jsonSetting.lectureJSONList("listGenre")
         # Icon Assistant
         iconAssistant = self.__arrtk.createImage(jsonSetting.lectureJSON("iconSoft"),tailleX=95,tailleY=95)
+        # String var
+        self.__varNameUser = StringVar(self.__windows)
 
         # Widget 
         # Main frame
         btnIcon = self.__arrtk.createButton(self.__mainCadre,width=100,height=100,image=iconAssistant)
-        btnAcceuilUser = self.__arrtk.createButton(self.__mainCadre,width=100,height=100,text="Utilisateur"
-                                                   ,ppolice="Arial",ptaille=taillePolice-2,pstyle="bold",command= lambda  : self.__viewUser())
+        btnAcceuilUser = self.__arrtk.createButton(self.__mainCadre, width=100, height=100, text="Utilisateur"
+                                                   , ppolice="Arial", ptaille=taillePolice-2, pstyle="bold", command= lambda  : self.__viewUserAcceuil())
         btnAcceuilMeteo = self.__arrtk.createButton(self.__mainCadre,width=100,height=100,text="Meteo"
                                                     ,ppolice="Arial",ptaille=taillePolice,pstyle="bold")
         btnAcceuilGPS = self.__arrtk.createButton(self.__mainCadre,width=100,height=100,text="GPS"
@@ -57,8 +64,30 @@ class CArreraGazelleUISix :
 
         # userFrame
         # Label
-        labelTitleUser = self.__arrtk.createLabel(self.__userFrame,text="Gestion utilisateur"
-                                                  ,ppolice="Arial",ptaille=taillePolice,pstyle="bold")
+        labelTitleUser = [self.__arrtk.createLabel(self.__userAcceuil,text="Gestion utilisateur"
+                                                  ,ppolice="Arial",ptaille=taillePolice,pstyle="bold"),
+                          self.__arrtk.createLabel(self.__userName,text="Nom de l'utilisateur",
+                                                   ppolice="Arial",ptaille=taillePolice,pstyle="bold"),
+                          self.__arrtk.createLabel(self.__userGenre,text="Genre de l'utilisateur"
+                                                   ,ppolice="Arial",ptaille=taillePolice,pstyle="bold")]
+        # Button
+        btnUserName = self.__arrtk.createButton(self.__userAcceuil,text="Nom\nde\nl'utilisateur",ppolice="Arial",
+                                                ptaille=taillePolice,pstyle="bold",command=lambda:self.__viewUserName())
+        btnUserGenre = self.__arrtk.createButton(self.__userAcceuil,text="Genre\nde\nl'utilisateur",ppolice="Arial",
+                                                 ptaille=taillePolice,pstyle="bold",command=lambda:self.__viewUserGenre())
+        btnValiderUserName = self.__arrtk.createButton(self.__userName,text="Valider",ppolice="Arial",
+                                                       ptaille=taillePolice,pstyle="normal",command=lambda:self.__saveUserName())
+        btnValiderUserGenre = self.__arrtk.createButton(self.__userGenre,text="Valider",ppolice="Arial",
+                                                        ptaille=taillePolice,pstyle="normal",command=lambda:self.__saveUserGenre())
+        btnRetourUserName = self.__arrtk.createButton(self.__userName, text="Retour", ppolice="Arial",
+                                                      ptaille=taillePolice, pstyle="normal",command=lambda:self.__viewUserAcceuil())
+        btnRetourUserGenre = self.__arrtk.createButton(self.__userGenre, text="Retour", ppolice="Arial",
+                                                       ptaille=taillePolice, pstyle="normal",command=lambda:self.__viewUserAcceuil())
+        # entry
+        self.__entryNameUser = self.__arrtk.createEntry(self.__userName, police="Arial", taille=taillePolice, width=200)
+        # option menu
+        menuUserGenre = self.__arrtk.createOptionMenu(self.__userGenre,value = listGenre,var = self.__varNameUser)
+
 
         # Affichage 
         btnIcon.place(x=20,y=20)
@@ -76,8 +105,20 @@ class CArreraGazelleUISix :
         # backFrame
         self.__arrtk.placeCenterRight(retourAcceuilBTN)
         # userFrame
-        self.__arrtk.placeTopCenter(labelTitleUser)
-    
+        for i in (range(0,len(labelTitleUser))):
+            self.__arrtk.placeTopCenter(labelTitleUser[i])
+
+        self.__arrtk.placeRightCenter(btnUserName)
+        self.__arrtk.placeLeftCenter(btnUserGenre)
+
+        self.__arrtk.placeBottomLeft(btnValiderUserName)
+        self.__arrtk.placeBottomLeft(btnValiderUserGenre)
+        self.__arrtk.placeBottomRight(btnRetourUserName)
+        self.__arrtk.placeBottomRight(btnRetourUserGenre)
+
+        self.__arrtk.placeCenter(self.__entryNameUser)
+        self.__arrtk.placeCenter(menuUserGenre)
+
     def active(self):
         self.__mainCadre.pack()
 
@@ -86,10 +127,40 @@ class CArreraGazelleUISix :
         self.__userFrame.pack_forget()
         self.__backFrame.pack_forget()
 
-    def __viewUser(self):
+    def __viewUserAcceuil(self):
         self.__clearAll()
+        self.__userName.pack_forget()
+        self.__userGenre.pack_forget()
+        self.__userAcceuil.pack()
         self.__userFrame.pack()
         self.__backFrame.pack()
+
+    def __viewUserName(self):
+        self.__userName.pack()
+        self.__userGenre.pack_forget()
+        self.__userAcceuil.pack_forget()
+
+    def __viewUserGenre(self):
+        self.__userName.pack_forget()
+        self.__userGenre.pack()
+        self.__userAcceuil.pack_forget()
+
+    def __saveUserName(self):
+        name = self.__entryNameUser.get()
+        if name == "":
+            messagebox.showerror("Parametre","Le nom de l'utilisateur ne peut pas etre vide")
+            return
+        else :
+            self.__gazelle.changeUserName(name)
+            messagebox.showinfo("Parametre","Le nom de l'utilisateur a bien été enregistré")
+            self.__entryNameUser.delete(0,END)
+            self.__viewUserAcceuil()
+
+    def __saveUserGenre(self):
+        genre = self.__varNameUser.get()
+        self.__gazelle.changeUserGenre(genre)
+        messagebox.showinfo("Parametre","Le genre de l'utilisateur a bien été enregistré")
+        self.__viewUserAcceuil()
 
     def __backAcceuil(self):
         self.__clearAll()
